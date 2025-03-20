c91f4fa431b59cf03e04a16a0a37030cd7d994d8
猫派 <nekohaaaa@outlook.jp>
2025-03-20 09:30:46 +0900

    main.py, from/import

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse
+import requests
+import urllib.parse
+
 @app.get("/verify", response_class=HTMLResponse)
 def get_form(seed=""):
     return requests.get(fr"{url}verify?seed={urllib.parse.quote(seed)}").text
